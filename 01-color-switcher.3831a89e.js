2d61abd0c00873ce00a360456c959a412b010659
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2023-11-05 09:06:09 +0000

    Deploying to gh-pages from @ Vitalina28/goit-js-hw-09@39f14b9a0a3d9912821770b62ed31ff1033e5a1a 🚀

---
+++ 01-color-switcher.3831a89e.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null;t.startBtn.addEventListener("click",(function(){n||(n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3))})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),n=null}))}();
+//# sourceMappingURL=01-color-switcher.3831a89e.js.map
---
+++ 01-color-switcher.e68373a7.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;t.startBtn.addEventListener("click",(function(){n||(n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),n=null}));
+//# sourceMappingURL=01-color-switcher.e68373a7.js.map
